7542942c07cde8c63dedf15d69354a6388d68e50
Paulo Roberto <paulodiastst@hotmail.com>
2023-05-01 21:55:30 -0300

    Criando Projeto Agenda Spring Boot

---
+++ src/main/java/br/com/futurodev/agenda/model/Contato.java
@@ -0,0 +1,101 @@
+package br.com.futurodev.agenda.model;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tab_contato")
+public class Contato implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 120)
+    private String nome;
+
+    @Column(length = 120)
+    private String apelido;
+
+    @Column(length = 12)
+    private String telefone;
+    private String email;
+    private String cidade;
+
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataCadastro;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contato contato = (Contato) o;
+        return Objects.equals(id, contato.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDate dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+}
